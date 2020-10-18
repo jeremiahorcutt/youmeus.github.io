16014a407e5c501e7ab0027527b2cf5ff6a49548
jeremiahorcutt <jeremiahorcutt@gmail.com>
2020-10-17 18:01:12 -0600

    s

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 var date = document.getElementById("date").value;
+
+this.$slideOut = $('#slideOut');
+
+// Slideout show
+this.$slideOut.find('.slideOutTab').on('click', function() {
+  $("#slideOut").toggleClass('showSlideOut');
+});
